6f7ad281aa9fed6c75576389ebb2ec5b407c2ed3
thanaprikdec-cmyk <thanaprikdec@gmail.com>
2026-06-04 09:34:11 +0700

    first commit

---
+++ lab1-web/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <h1>ยินดีต้อนรับเข้าสู่เว็บไซต์</h1>
+      <p>โดย ปรมะ เกิดผลหลาก</p>
+    </div>
+  );
+}
